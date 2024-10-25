85418a127c1a6c9ff8155a5359762ff006a2595d
lsandaruwanjayas <fgod406@gmail.com>
2024-10-25 17:00:28 +0530

    Player button funtions, display current song info

--- src/script/main.js
+++ src/script/main.js
@@ -18,36 +18,35 @@ const allSongs = [
     title: "Perfect Beauty",
     artist: "Good_B_Music",
     duration: "7.20",
-    src: "./src/audio/perfect-beauty.mp3",
-   
+    src: "./src/audio/perfect-beauty.mp3"
   },
   {
     id: 2,
     title: "Ethereal Vistas",
     artist: "Denys Brodovskyi",
     duration: "4.01",
-    src: "./src/audio/ethereal-vistas.mp3",
+    src: "./src/audio/ethereal-vistas.mp3"
   },
   {
     id: 3,
     title: "Movement",
     artist: "SoulProdMusic",
     duration: "2.35",
-    src: "./src/audio/movement.mp3",
+    src: "./src/audio/movement.mp3"
   },
   {
     id: 4,
     title: "Amalgam",
     artist: "Rockot",
     duration: "4.14",
-    src: "./src/audio/amalgam.mp3",
+    src: "./src/audio/amalgam.mp3"
   },
   {
     id: 5,
     title: "In Slow Motion",
     artist: "soundbay",
     duration: "1.58",
-    src: "./src/audio/in-slow-motion.mp3",
+    src: "./src/audio/in-slow-motion.mp3"
   },
   {
     id: 6,
@@ -61,14 +60,14 @@ const allSongs = [
     title: "Night Detective",
     artist: "Amaksi",
     duration: "1.55",
-    src: "./src/audio/night-detective.mp3",
+    src: "./src/audio/night-detective.mp3"
   },
   {
     id: 8,
     title: "Nightfall",
     artist: "SoulProdMusic",
     duration: "2.21",
-    src: "./src/audio/nightfall.mp3",
+    src: "./src/audio/nightfall.mp3"
   },
   {
     id: 9,
@@ -89,7 +88,7 @@ const allSongs = [
     title: "No Place To Go",
     artist: "SergePavkinMusic",
     duration: "5.37",
-    src: "./src/audio/no-place-to-go.mp3",
+    src: "./src/audio/no-place-to-go.mp3"
   },
 ];
 
@@ -101,10 +100,33 @@ let userData = {
   songCurrentTime: 0,
 };
 
+const playSong = (id) => {
+  const song = userData?.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
+
+  if (!userData?.currentSong || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.songCurrentTime;
+  }
+  userData.currentSong = song;
+  playButton.classList.add("playing");
+  audio.play();
+};
+
+
+const pauseSong = () => {
+  userData.songCurrentTime = audio.currentTime;
+  playButton.classList.remove("playing");
+  audio.pause();
+}
+
 const renderSongs = (array) => {
-  const songsHTML = array.map((song) => {
-    return `<li id="song-${song.id}" class="playlist-song"></li>
-     <button class="playlist-song-info">
+  const songsHTML = array
+    .map((song) => {
+      return `<li id="song-${song.id}" class="playlist-song"></li>
+     <button class="playlist-song-info" onclick="playSong(${song.id})">
      <span class="playlist-song-title">${song.title}</span>
       <span class="playlist-song-artist">${song.artist}</span>
       <span class="playlist-song-duration">${song.duration}</span>
@@ -116,21 +138,33 @@ const renderSongs = (array) => {
           <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
       </button>
      `;
-  }).join("");
+    })
+    .join("");
   playlistSongs.innerHTML = songsHTML;
 };
 
+playButton.addEventListener("click", () => {
+  if(userData?.currentSong === null){
+    playSong(userData?.songs[0].id);
+  }
+  else{
+    playSong(userData?.currentSong.id)
+  }
+});
+
+pauseButton.addEventListener("click", pauseSong)
+
 const sortSongs = () => {
-  userData?.songs.sort((a,b)=>{
-    if(a.title < b.title){
+  userData?.songs.sort((a, b) => {
+    if (a.title < b.title) {
       return -1;
     }
-    if(a.title > b.title){
-      return 1
+    if (a.title > b.title) {
+      return 1;
     }
     return 0;
-  })
-  return userData?.songs
-} 
+  });
+  return userData?.songs;
+};
 
 renderSongs(sortSongs());
